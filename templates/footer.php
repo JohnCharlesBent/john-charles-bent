54d2dc782510e7d7d9fb8c8e63610bffe04a56df
JohnCharlesBent <johnbentdesign@gmail.com>
2019-05-30 14:14:26 -0400

    fixed footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,9 @@
+<?php
+	$email = get_field('email', 'options');
+	$email = 'mailto:'.$email;
+	$github = get_field('github', 'options');
+?>
+
 <footer>
 
 	<div class="footer-branding__name">
@@ -9,22 +15,20 @@
 			<div class="footer-wave_line">
 				<?php get_template_part('templates/svg/wave'); ?>
 			</div>
-	</div>
 
-	<?php
-		$email = get_field('email', 'options');
-		$email = 'mailto:'.$email;
-		$github = get_field('github', 'options');
-	?>
-
-	<div class="footer-branding__social-media">
-		<div class="social-title">Stay Connected</div>
-
-		<div class="icons">
-			<a href="<?php echo $email; ?>" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-			<a href="<?php echo $github; ?>" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
-		</div>
+			<div class="footer-branding__social-media">
+				<div class="social-title">Stay Connected</div>
+				<div class="icons">
+					<a href="<?php echo $email; ?>" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+					<a href="<?php echo $github; ?>" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+					<a href="https://www.linkedin.com/in/john-b-b8a3a486/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				</div>
+			</div>
 
 	</div>
+
+
+
+
 
 </footer>
